1f068a2cef1c744996a86c06629ca69e27aaa5d7
Nandan <nanspyro@gmail.com>
2022-06-18 16:30:30 +0530

    added auth and image pick

---
+++ crime_app/lib/home.dart
@@ -0,0 +1,64 @@
+import 'package:crime_app/authentication.dart';
+import 'package:crime_app/login.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+import 'imagefromgallery.dart';
+
+enum ImageSourceType { gallery, camera }
+
+class Home extends StatelessWidget {
+  void _handleURLButtonPress(BuildContext context, var type) {
+    Navigator.push(context,
+        MaterialPageRoute(builder: (context) => ImageFromGalleryEx(type)));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            AuthenticationHelper()
+                .signOut()
+                .then((_) => Navigator.pushReplacement(
+                      context,
+                      MaterialPageRoute(builder: (contex) => Login()),
+                    ));
+          },
+          child: Icon(Icons.logout),
+          tooltip: 'Logout',
+        ),
+        appBar: AppBar(
+          title: Text("Image Picker Example"),
+        ),
+        body: Center(
+          child: Column(
+            children: [
+              MaterialButton(
+                color: Colors.blue,
+                child: Text(
+                  "Pick Image from Gallery",
+                  style: TextStyle(
+                      color: Colors.white70, fontWeight: FontWeight.bold),
+                ),
+                onPressed: () {
+                  _handleURLButtonPress(context, ImageSourceType.gallery);
+                },
+              ),
+              MaterialButton(
+                color: Colors.blue,
+                child: Text(
+                  "Pick Image from Camera",
+                  style: TextStyle(
+                      color: Colors.white70, fontWeight: FontWeight.bold),
+                ),
+                onPressed: () {
+                  _handleURLButtonPress(context, ImageSourceType.camera);
+                },
+              ),
+            ],
+          ),
+        ));
+  }
+}
