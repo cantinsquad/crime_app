813bda00ea9551d513c6b3be78f1234cb821798d
Nandan <nanspyro@gmail.com>
2022-06-19 19:56:29 +0530

    location fir

--- crime_app/lib/home.dart
+++ crime_app/lib/home.dart
@@ -3,6 +3,7 @@ import 'package:crime_app/login.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:location/location.dart';
 
 import 'imagefromgallery.dart';
 
